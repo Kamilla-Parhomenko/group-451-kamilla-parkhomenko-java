4d1ea4277c751a57a31770545d69426b544c0c72
Kamilla-Parhomenko <parhomenkokamilla@gmail.com>
2026-03-13 22:00:43 +0400

    classworr

---
+++ src/classwork_9/Main.java
@@ -0,0 +1,10 @@
+package classwork_9;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Task1 t = new Task1();
+		t.sum(6,7);
+	}
+}
---
+++ src/classwork_9/Task1.java
@@ -0,0 +1,12 @@
+package classwork_9;
+
+public class Task1 {
+
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public void even(int number) {
+		
+	}
+}
