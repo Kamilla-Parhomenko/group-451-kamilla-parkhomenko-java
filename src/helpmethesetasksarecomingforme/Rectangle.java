dca320324016b1a1d1b40cfc82f2e1d5578b97d0
Kamilla-Parhomenko <parhomenkokamilla@gmail.com>
2026-04-21 23:01:24 +0400

    ok

--- src/helpmethesetasksarecomingforme/Rectangle.java
+++ src/helpmethesetasksarecomingforme/Rectangle.java
@@ -2,4 +2,13 @@ package helpmethesetasksarecomingforme;
 
 public class Rectangle {
 
+	double width, height;
+	
+	double area() {
+		return width*height;
+	}
+	
+	double perimeter() {
+		return (width + height)*2;
+	}
 }
