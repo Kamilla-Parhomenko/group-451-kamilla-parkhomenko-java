d8f06486e80b1e3669d699e8051d4b4fffd8220d
Kamilla-Parhomenko <parhomenkokamilla@gmail.com>
2026-05-24 21:28:22 +0400

    cw

---
+++ src/vvvvvgbsdclasswork/Taaskkkk.java
@@ -0,0 +1,40 @@
+package vvvvvgbsdclasswork;
+
+public class Taaskkkk {
+
+	public static void main(String[] args) {
+		
+		String name = "Christopher Grey";
+		System.out.println("Hello, " + name + "!");
+		
+		int year = 2026;
+		int age = 15;
+		
+		System.out.println(year-age);
+		
+		double h = 7;
+		double w = 6;
+		System.out.println(h*w);
+		
+		int a = 5;
+		int b = 10;
+		// a kak
+		
+		int min = 150;
+		double hour = min/60;
+		
+		int price = 450;
+		int endirim = 30;
+		System.out.println((price/100)*30);
+		
+		double r = 6;
+		double l = 2*r*Math.PI;
+		double s = Math.pow(6, 2)*Math.PI;
+		
+		
+		String firstName = "name";
+		String lastName = "surname";
+		
+		String fullName = firstName + " " + lastName;
+	}
+}
